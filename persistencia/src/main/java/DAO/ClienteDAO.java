598b35962020933a74c28b1c44d4dc2ca3206443
munos <munos@bicho>
2026-03-27 03:24:41 -0700

    validadores registro clientes regex clienteDAO+

--- persistencia/src/main/java/DAO/ClienteDAO.java
+++ persistencia/src/main/java/DAO/ClienteDAO.java
@@ -6,14 +6,17 @@ package DAO;
 
 import conexion.ConexionBD;
 import entidades.Cliente;
+import entidades.ClienteFrecuente;
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceException;
 
 /**
  *
  * @author munos
  */
 public class ClienteDAO {
-public Cliente guardar(Cliente cliente) {
+    
+    public ClienteFrecuente guardar(ClienteFrecuente cliente) {
         EntityManager em = ConexionBD.crearConexion();
         try {
             em.getTransaction().begin();
@@ -24,9 +27,46 @@ public Cliente guardar(Cliente cliente) {
             if (em.getTransaction().isActive()) {
                 em.getTransaction().rollback();
             }
+            throw new PersistenceException("error al guardar");
+        } finally {
+            em.close();
+        }
+    }
+    public boolean eliminar(Long id) {
+        EntityManager em = ConexionBD.crearConexion();
+        try {
+            ClienteFrecuente cliente = em.find(ClienteFrecuente.class, id);
+            if (cliente == null) {
+                return false;
+            }
+            em.getTransaction().begin();
+            em.remove(cliente);
+            em.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            throw new PersistenceException("error al eliminar");
+        } finally {
+            em.close();
+        }
+    }
+     public ClienteFrecuente editar(ClienteFrecuente cliente) {
+        EntityManager em = ConexionBD.crearConexion();
+
+        try {
+            em.getTransaction().begin();
+            ClienteFrecuente actualizado = em.merge(cliente);
+            em.getTransaction().commit();
+            return actualizado;
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
             throw e;
         } finally {
             em.close();
-        }    
-}
-}
+        }
+    }
+     }
